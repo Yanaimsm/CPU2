02231619f7d2bbe43c05828ce9cf3650a1db3852
Yanaimsm <yanaimason94@gmail.com>
2025-04-20 13:53:02 +0300

    added enable improvemnets

--- DUT/top.vhd
+++ DUT/top.vhd
@@ -2,6 +2,7 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 USE ieee.std_logic_unsigned.all;
 USE work.aux_package.all;
+
 ------------------------------------------------------------------
 entity top is
 	generic ( n : positive := 8 ); 
@@ -16,7 +17,7 @@ architecture arc_sys of top is
 	SIGNAL cnt_slow_q : std_logic_vector(n-1 DOWNTO 0); -- synchronous register to store slow counter process output
 	SIGNAL control_r : std_logic_vector(n-1 DOWNTO 0); -- control register used for updating current counter bound
 	signal fast_done_r    : boolean; -- signal to show that a cycle has completed
-	signal enable_r       : boolean; -- signal that allows count to proceed.
+    signal enable_r       : boolean := true;  -- Enable flag, initially enabled
  
 begin
 	----------------------------------------------------------------
@@ -30,15 +31,17 @@ begin
 	----------------------------------------------------------------
 	----------------------- synchronous part -----------------------
 	
-	elsif rising_edge(clk_i) then
-			if fast_done_r and enable_r then  -- If the fast counter has reached the current limit set by control_r and process is enabled
-				cnt_fast_q <= (others => '0');  -- Reset the fast counter to zero
-			elsif enable_r then
-				cnt_fast_q <= cnt_fast_q + 1;  -- Otherwise, increment the fast counter
-			else cnt_fast_q <= cnt_fast_q; --hold final value
-			end if;
-		end if;
-	end process;
+        elsif rising_edge(clk_i) then
+            if fast_done_r and enable_r then  -- Reset fast counter if cycle is done and enabled
+                cnt_fast_q <= (others => '0');
+            elsif enable_r then
+                cnt_fast_q <= cnt_fast_q + 1;  -- Otherwise, increment fast counter
+            else 
+                cnt_fast_q <= cnt_fast_q;  -- Hold fast counter value if not enabled
+            end if;
+        end if;
+    end process;
+
 	
 	----------------------------------------------------------------
 	---------------------- slow counter process --------------------
@@ -50,38 +53,29 @@ begin
 	----------------------- asynchronous part ----------------------
 		if rst_i = '1' then
 			cnt_slow_q <= (others => '0');
+					enable_r   <= true;  -- enable everything after reset
 	----------------------------------------------------------------
 	----------------------- synchronous part -----------------------			
-		elsif rising_edge(clk_i) then  
-			if fast_done_r then  -- Only proceed if the fast counter has finished its current cycle
-				if cnt_slow_q < upperBound_i then  -- If the slow counter hasn't reached the upper bound yet
-					cnt_slow_q <= cnt_slow_q + 1;  -- Increment the slow counter
-				elsif repeat_i = '1' then  -- Else, if repeat is requested
-					cnt_slow_q <= (others => '0');  -- Reset the slow counter to zero
-				else enable_r = 0; -- signal flag to stop fast count process when done
-				end if;
-			else
-				cnt_slow_q <= cnt_slow_q;  -- Otherwise, hold the current value (do nothing)
-				
-			end if;  
-		end if;  
-	end process;  
-
+        elsif rising_edge(clk_i) then  
+            if fast_done_r then  -- Proceed only if fast counter cycle is done
+                if cnt_slow_q < upperBound_i then  -- Slow counter not yet at upper bound
+                    cnt_slow_q <= cnt_slow_q + 1;
+                elsif repeat_i = '1' then  -- If repeat is requested
+                    cnt_slow_q <= (others => '0');  -- Reset the slow counter to zero
+                else
+                    enable_r <= false;  -- Disable counting after last round if repeat is 0
+                end if;
+            end if;
+        end if;
+    end process;
 
 	---------------------------------------------------------------
 	--------------------- combinational part ----------------------
 	---------------------------------------------------------------
-	fast_done_r <= (cnt_fast_q = control_r);
-	control_r <= cnt_slow_q;
-	count_o <= cnt_fast_q;
-	busy_o <= '1' when (cnt_slow_q < upperBound_i or repeat_i = '1') and rst_i = '0' else '0'; -- turn off only when not repeating and finished counting
+    fast_done_r <= (cnt_fast_q = control_r);  -- Fast counter done when equal to control_r
+    control_r <= cnt_slow_q;  -- Set the control register to the slow counter value
+    count_o <= cnt_fast_q;  -- Output the current fast counter value
+    busy_o <= '1' when (cnt_slow_q < upperBound_i or repeat_i = '1') and rst_i = '0' else '0';  -- Busy if not finished or repeat is active
 
 	----------------------------------------------------------------
 end arc_sys;
-
-
-
-
-
-
-
