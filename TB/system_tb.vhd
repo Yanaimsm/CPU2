c1bbede2068c010e82225d7f163f3a062fa00611
Yanaimsm <yanaimason94@gmail.com>
2025-04-23 20:09:18 +0300

    removed useless comments

--- TB/system_tb.vhd
+++ TB/system_tb.vhd
@@ -27,33 +27,33 @@ begin
 
 	gen_upperBound : process
 	begin
-		upperBound <= (2 => '0', 0 => '1', others => '0');  -- changed starting pattern
+		upperBound <= (2 => '0', 0 => '1', others => '0');  
 		for i in 0 to 1 loop
-			wait for 900 ns;  -- slightly changed delay
-			upperBound <= upperBound + 2;  -- changed increment
+			wait for 900 ns;  
+			upperBound <= upperBound + 2;  
 		end loop;
 		
-		wait for 700 ns;  -- single delay instead of loop
-		upperBound <= upperBound - 3;  -- changed decrement value
+		wait for 700 ns;  
+		upperBound <= upperBound - 3;  
 		
 		for i in 0 to 2 loop
-			wait for 750 ns;  -- slightly changed delay
+			wait for 750 ns;  
 			upperBound <= upperBound + 1;
 		end loop;
 	end process;
 
 	gen_rst : process
 	begin
-		rst <= '1'; wait for 120 ns;  -- modified timing
-		rst <= '0'; wait for 900 ns; -- modified timing
-		rst <= '1'; wait for 180 ns; -- modified timing
+		rst <= '1'; wait for 120 ns;  
+		rst <= '0'; wait for 900 ns; 
+		rst <= '1'; wait for 180 ns; 
 		rst <= '0'; wait;
 	end process;
 
 	gen_repeat : process
 	begin
 		repeat <= '1';
-		wait for 2200 ns;  -- changed duration
+		wait for 2200 ns;  
 		repeat <= '0';
 		wait;
 	end process;
